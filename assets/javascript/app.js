da336fbe270c43a26b93d4c09e0a428f1ffcaf8b
Christina Wu <CHRISTINASARAHWU@GMAIL.COM>
2018-05-26 02:40:29 -0700

    added email function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,16 @@
-$(document).ready(function(){
+$(document).ready(function () {
+
+    // On click to send an email //
+    $('#gmailLogo').on('click', function (event) {
+        var email = 'ChristinaSarahWu@gmail.com';
+        window.location = 'mailto:' + email + '?';
+    }),
+
+        console.log("hi");
     M.AutoInit();
     $('.sidenav').sidenav();
     $('.scrollspy').scrollSpy({
         scrollOffset: 100,
-    },
-    
-  console.log("hi"));
-  });
+    });
+
+});
